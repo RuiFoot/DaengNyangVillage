df9b2348a3c8c89fcec4b41146435086b24c1504
smy001121 <67827154+smy001121@users.noreply.github.com>
2024-04-12 14:23:22 +0900

    하이

--- src/main/java/com/myspring/daengnyang/Home/controller/HomeController.java
+++ src/main/java/com/myspring/daengnyang/Home/controller/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping(value = "/test")
     public String home(){
+        System.out.println("하이");
         return "안녕";
     }
 }
