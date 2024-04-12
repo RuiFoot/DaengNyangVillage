e9c79ac7221f9bc5298183b09b897242321f328b
RuiFoot <jin14500@naver.com>
2024-04-12 10:33:06 +0900

    setting

---
+++ src/main/java/com/myspring/daengnyang/Home/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.myspring.daengnyang.Home.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping(value = "/test")
+    public String home(){
+        return "안녕";
+    }
+}
