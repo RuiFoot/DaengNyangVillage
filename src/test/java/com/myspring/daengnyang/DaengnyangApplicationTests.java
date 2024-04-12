56d784a6d5ebdb395e93fb3c499b4c03952ee5cf
RuiFoot <jin14500@naver.com>
2024-04-12 10:48:37 +0900

    setting

---
+++ src/test/java/com/myspring/daengnyang/DaengnyangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myspring.daengnyang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaengnyangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
