901b6ba9322a4ffdd4a30ba3e3396b5f4246d6d3
RuiFoot <jin14500@naver.com>
2024-04-18 10:57:35 +0900

    2024.04.18 브랜치 나눔

--- src/main/java/com/myspring/daengnyang/config/SecurityConfig.java
+++ src/main/java/com/myspring/daengnyang/config/SecurityConfig.java
@@ -10,17 +10,21 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final CorsConfigurationSource corsConfigurationSource;
+
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http
                 .csrf((csrf) -> csrf
                         .ignoringRequestMatchers(new AntPathRequestMatcher("/**"))) // 403 error를 막기 위해서 사용
+                .cors(cors -> cors.configurationSource(corsConfigurationSource))
                 .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
                         .requestMatchers(new AntPathRequestMatcher("/**")).permitAll()) // 모든 사람이 접근 권한을 가지게 하기 위해 사용
 
