f7a923bcccc6fc1f4d7fa5e3774a7b11798e6ef9
RuiFoot <jin14500@naver.com>
2024-04-22 14:01:22 +0900

    2024.04.22 카카오 로그인/회원가입, 카카오 로그아웃/연결끊기 기능 구현

--- src/main/java/com/myspring/daengnyang/member/controller/MemberController.java
+++ src/main/java/com/myspring/daengnyang/member/controller/MemberController.java
@@ -1,6 +1,7 @@
 package com.myspring.daengnyang.member.controller;
 
 import com.myspring.daengnyang.member.service.MemberServiceImpl;
+import com.myspring.daengnyang.member.service.OAuthServiceImpl;
 import com.myspring.daengnyang.member.vo.MemberVO;
 import com.myspring.daengnyang.member.vo.SignupForm;
 import jakarta.servlet.http.HttpServletRequest;
@@ -25,26 +26,35 @@ public class MemberController {
 
     private final MemberServiceImpl memberService;
     private final PasswordEncoder passwordEncoder;
+
+    private final OAuthServiceImpl oAuthService;
     @Autowired
-    public MemberController(MemberServiceImpl memberService, PasswordEncoder passwordEncoder) {
+    public MemberController(MemberServiceImpl memberService, PasswordEncoder passwordEncoder, OAuthServiceImpl oAuthService) {
         this.memberService = memberService;
         this.passwordEncoder = passwordEncoder;
+        this.oAuthService = oAuthService;
     }
 
-
-    @GetMapping("")
-    public String member(){
-        return "member";
-    }
+    /**\
+     * 회원가입
+     * @param signupForm : 회원가입에 필요한 정보들
+     * @return
+     */
     @PostMapping("/signup")
     @ResponseBody
-    public String createMember(@RequestBody SignupForm signupForm){
+    public boolean createMember(@RequestBody SignupForm signupForm){
         log.info("일반 회원가입 실행");
         log.info(signupForm.toString());
         memberService.createMember(signupForm);
-        return "redirect:/";
+        return true;
     }
 
+    /**
+     * 일반 로그인
+     * @param request : 이메일, password 전송
+     * @param httpRequest : request의 session을 활용하기 위한 파라미터
+     * @return Response
+     */
     @PostMapping("/login")
     public ResponseEntity<MemberVO> login(
             @RequestBody Map<String, String> request,
@@ -72,14 +82,55 @@ public class MemberController {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
         }
     }
+
+    /**
+     * 중복확인 : email
+     * @param email
+     * @return boolean
+     */
     @GetMapping("/duplicationE")
     public boolean duplicationEmail(@RequestParam("email") String email){
         return memberService.getDuplicationEmail(email);
     }
 
+    /**
+     * 중복확인 : 닉네임
+     * @param nickname
+     * @return boolean
+     */
     @GetMapping("/duplicationN")
     public boolean duplicationNickname(@RequestParam("nickname") String nickname) {
         System.out.print(memberService.duplicationNickname(nickname));
         return memberService.duplicationNickname(nickname);
     }
+
+    //-------------------------------------------------------------------------------------------
+
+    /**
+     * access token을 통해 카카오 개인정보 체크
+     * @param code
+     * https://kauth.kakao.com/oauth/authorize?client_id=db0c282555cc32e78ecbce031761fc83&redirect_uri=http://localhost:8080/member/oauth/kakao&response_type=code
+     *
+     * https://kauth.kakao.com/oauth/logout?client_id=db0c282555cc32e78ecbce031761fc83&logout_redirect_uri=http://localhost:8080/member/oauth/kakao/logout
+     */
+    @GetMapping("/oauth/kakao")
+    public void kakaoCallback(@RequestParam String code){
+        log.info("code : " + code);
+        String accessToken = oAuthService.getKakaoAccessToken(code);
+        System.out.println(accessToken);
+        String loginResult = oAuthService.getUserInfo(accessToken);
+        log.info("로그인 정보 : " + loginResult);
+
+        log.info("로그인이 정상 처리 되었습니다.");
+    }
+    @GetMapping("/oauth/kakao/logout")
+    public void kakaoLogoutCallback(){
+        Long target_id = 3428886536L;
+        log.info("kakaoLogout");
+        String logoutResult = oAuthService.kakaoLogout(target_id);
+        log.info(logoutResult + "로그아웃이 정상 처리 되었습니다.");
+    }
+
+    //---------------------------------------------------------------------------------------------------------------------
+
 }
