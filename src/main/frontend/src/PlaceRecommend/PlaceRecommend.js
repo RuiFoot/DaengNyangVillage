fb8c1e5ab6ba8c17adb97baa0a551120d02e63a5
lbeen <lsb96827@naver.com>
2024-04-23 14:59:16 +0900

    modified:   src/main/frontend/src/PlaceRecommend/PlaceRecommend.js

--- src/main/frontend/src/PlaceRecommend/PlaceRecommend.js
+++ src/main/frontend/src/PlaceRecommend/PlaceRecommend.js
@@ -113,48 +113,58 @@ function PlaceRecommend() {
         const container = document.getElementById('map');
         const options = {
             center: new kakao.maps.LatLng(33.450701, 126.570667),
-            lever: 5
+            lever: 3
         };
         const map = new kakao.maps.Map(container, options);
         
         var positions = [
             {
-                title: '카카오', 
+                content: '<div>카카오</div>',  
                 latlng: new kakao.maps.LatLng(33.450705, 126.570677)
             },
             {
-                title: '생태연못', 
+                content: '<div>생태연못</div>', 
                 latlng: new kakao.maps.LatLng(33.450936, 126.569477)
             },
             {
-                title: '텃밭', 
+                content: '<div>텃밭</div>',  
                 latlng: new kakao.maps.LatLng(33.450879, 126.569940)
             },
             {
-                title: '근린공원',
+                content: '<div>근린공원</div>',
                 latlng: new kakao.maps.LatLng(33.451393, 126.570738)
             }
         ];
         
-        // 마커 이미지의 이미지 주소입니다
-        var imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png"; 
-            
         for (var i = 0; i < positions.length; i ++) {
-            
-            // 마커 이미지의 이미지 크기 입니다
-            var imageSize = new kakao.maps.Size(24, 35); 
-            
-            // 마커 이미지를 생성합니다    
-            var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize); 
-            
-            // 마커를 생성합니다
             var marker = new kakao.maps.Marker({
                 map: map, // 마커를 표시할 지도
                 position: positions[i].latlng, // 마커를 표시할 위치
                 title : positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-                image : markerImage // 마커 이미지 
             });
+            // 마커에 표시할 인포윈도우를 생성합니다 
+            var infowindow = new kakao.maps.InfoWindow({
+                content: positions[i].content // 인포윈도우에 표시할 내용
+            });
+            kakao.maps.event.addListener(marker, 'mouseover', makeOverListener(map, marker, infowindow));
+            kakao.maps.event.addListener(marker, 'mouseout', makeOutListener(infowindow));
         }
+        // 인포윈도우를 표시하는 클로저를 만드는 함수입니다 
+        function makeOverListener(map, marker, infowindow) {
+            return function() {
+                infowindow.open(map, marker);
+            };
+        }
+
+        // 인포윈도우를 닫는 클로저를 만드는 함수입니다 
+        function makeOutListener(infowindow) {
+            return function() {
+                infowindow.close();
+            };
+        }
+        
+
+
     }, [])
 
     return (
@@ -224,7 +234,7 @@ function PlaceRecommend() {
                         </CheckBoxs>
                         <Map>
                             <div id="map" style={{
-                                width: '500px',
+                                width: '100%',
                                 height: '500px'
                             }}></div>
                         </Map>
