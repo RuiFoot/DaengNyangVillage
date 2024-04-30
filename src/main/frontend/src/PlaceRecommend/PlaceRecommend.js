39aa4cff2cea100f898e148edc88f2cc4a611b77
lbeen <lsb96827@naver.com>
2024-04-30 11:24:33 +0900

    modified:   src/main/frontend/src/PlaceRecommend/PlaceRecommend.js

--- src/main/frontend/src/PlaceRecommend/PlaceRecommend.js
+++ src/main/frontend/src/PlaceRecommend/PlaceRecommend.js
@@ -2,7 +2,7 @@ import Bumper from "../layout/Bumper";
 import styled from "styled-components";
 import { GoDotFill } from "react-icons/go";
 import './PlaceRecommendStyle.css'
-import { useEffect, useState } from "react";
+import React ,{ useEffect, useState } from "react";
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Button from 'react-bootstrap/Button';
@@ -79,12 +79,21 @@ const CheckBoxLabel = styled.label`
 
 const { kakao } = window;
 
+
+
+
 //6개
 let wideHotPlaceArr = [["춘천 삼악산 호수 케이블카", "강원 춘천시 스포츠타운길 245", "리드줄, 매너벨트 필수 착용", "https://files.ban-life.com/content/2024/04/body_1711961501.jpg"], ["청도 프로방스", "경북 청도군 화양읍 이슬미로 272-23", "리쉬필수! 댕댕이들은 모~두 입장 가능!", "https://files.ban-life.com/content/2024/04/body_1712306959.jpg"], ["양평 레몬과오렌지", "경기도 양평군 단월면 양동로 229", "독채 숙소라 마당에서 프라이빗하게 뛰뛰하고 우리끼리 즐길 수 있어요", "https://files.ban-life.com/content/2024/04/body_1712301999.jpg"], ["캔버스 스테이 외관", "부산광역시 해운대구 해운대해변로197번길 13", " 강아지 수영장+루프탑", "https://files.ban-life.com/content/2024/04/body_1712316724.jpg"], ["태안 코리아 플라워 파크", "충남 태안 안면읍 꽃지해안로 400", "견종 무관하게 모두 동반 가능해요", "https://files.ban-life.com/content/2024/04/body_1712595967.jpg"], ["감성스테이 산아래", "충청남도 당진시 송산면 칠절길 95-17", "견종, 무게 제한 없음", "https://files.ban-life.com/content/2024/04/body_1712079373.jpg"]]
 
 //let categoryList = ["동물병원", "동물약국", "반려동물용품", "미용", "위탁관리", "식당", "카페", "호텔", "팬션", "여행지", "박물관", "문예회관"]
 
+
 function PlaceRecommend() {
+
+    const [map, setMap] = useState(null);
+    const [markers, setMarkers] = useState([]);
+
+
     const [categoryList,setCategoryList] = useState([]);
     const [address,setAddress] = useState([]);
     const [windowSize, setWindowSiz] = useState(window.innerWidth);
@@ -104,54 +113,84 @@ function PlaceRecommend() {
         axios.get(`${baseUrl}/animal`)
             .then((res) => {
                 setCategoryList(res.data)
-                console.log(res.data)
+                // console.log(res.data)
             }).catch(error => {
                 console.error('Request failed : ', error);
         })
     }, []);
 
     useEffect(() => {
-        axios.get(`${baseUrl}/animal/location/%EB%8F%99%EB%AC%BC%EC%95%BD%EA%B5%AD`)
+        let category ="미술관"; 
+        // axios를 사용하여 데이터 가져오기
+        axios.get(`${baseUrl}/animal/location/${category}`)
             .then((res) => {
-                setAddress(res.data)
-                console.log(res.data)
+                setAddress(res.data);
+                //console.log(res.data);
             }).catch(error => {
                 console.error('Request failed : ', error);
-        })
-
-
+        });
+    }, []);
+    
+    useEffect(() => {
+        // 카카오맵 초기화
         const container = document.getElementById('map');
         const options = {
-            center: new kakao.maps.LatLng(37.335889, 126.584063),
+            center: new kakao.maps.LatLng(37.5664056, 126.9778222),
             level: 10
         };
-        const map = new kakao.maps.Map(container, options);
-
-        var positions = [];
-        for(var j=0; j<Object.keys(address).length; j++){
-            var content = {
-                title: address[j].facilityName,
-                LatLng: new kakao.maps.LatLng(address[j].latitude, address[j].longitude),
-                roadAddress: address[j].roadAddress,
-            }
-            positions.push(content);
-        };
-        console.log(positions);
-
-
-        
-        for (var i = 0; i < positions.length; i++) {
-            var marker = new kakao.maps.Marker({
-                map: map, // 마커를 표시할 지도
-                position: positions[i].LatLng, // 마커를 표시할 위치
-                title: positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-                
+        const newMap = new kakao.maps.Map(container, options);
+        setMap(newMap);
+    }, []);
+    
+    useEffect(() => {
+        // 주소 정보를 이용하여 마커 표시
+        if (address && Object.keys(address).length > 0 && map) {
+            // 이전에 생성된 마커들 제거
+            markers.forEach(marker => {
+                marker.setMap(null);
             });
+            // 새로운 마커들 생성
+            var newMarkers = [];
+            for(var j=0; j<Object.keys(address).length; j++){
+                var content = {
+                    title: address[j].facilityName,
+                    LatLng: new kakao.maps.LatLng(address[j].latitude, address[j].longitude),
+                    roadAddress: address[j].roadAddress,
+                }
+                var newMarker = new kakao.maps.Marker({
+                    map: map,
+                    position: content.LatLng,
+                    title: content.title
+                });
+                newMarkers.push(newMarker);
+                // 마커 클릭 시 오버레이 표시
+                (function(marker, place) {
+                    kakao.maps.event.addListener(marker, 'click', function() {
+                        var overlay = new kakao.maps.CustomOverlay({
+                            content: '<div class="wrap">' +
+                                '    <div class="info">' +
+                                '        <div class="title">' + place.title + '</div>' +
+                                '        <div class="body">' +
+                                '            <div class="desc">' +
+                                '                <div class="ellipsis">' + place.roadAddress + '</div>' +
+                                '            </div>' +
+                                '        </div>' +
+                                '    </div>' +
+                                '</div>',
+                            map: map,
+                            position: marker.getPosition()
+                        });
+                        kakao.maps.event.addListener(map, 'click', function(mouseEvent) {
+                            overlay.setMap(null);
+                        });
+                        overlay.setMap(map);
+                    });
+                })(newMarker, content);
+            }
+            // 새로운 마커들을 저장하여 나중에 제거할 수 있도록 함
+            setMarkers(newMarkers);
         }
-
-
-
-    }, [])
+    }, [address, map]);
 
     return (
         <Container>
