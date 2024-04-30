c03f3a570a061843b0e6293c301595a55100e17a
RuiArmpit <jin14500@naver.com>
2024-04-30 12:05:14 +0900

    fix : 상빈 코드 merge

--- src/main/frontend/src/placeRecommend/placeRecommend.js
+++ src/main/frontend/src/placeRecommend/placeRecommend.js
@@ -1,7 +1,7 @@
-import Bumper from "../layout/Bumper";
+import Bumper from "../layout/bumper";
 import styled from "styled-components";
 import { GoDotFill } from "react-icons/go";
-import './PlaceRecommendStyle.css'
+import './placeRecommendStyle.css'
 import React ,{ useEffect, useState } from "react";
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
