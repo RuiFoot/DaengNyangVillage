c06272d4f5752e396e3747fed516eb073d6090c1
smy001121 <67827154+smy001121@users.noreply.github.com>
2024-05-02 11:19:44 +0900

    코드 정리

--- src/main/java/com/myspring/daengnyang/member/controller/MemberController.java
+++ src/main/java/com/myspring/daengnyang/member/controller/MemberController.java
@@ -3,7 +3,7 @@ package com.myspring.daengnyang.member.controller;
 import com.myspring.daengnyang.animal.vo.AnimalLocationVO;
 import com.myspring.daengnyang.board.vo.BoardVO;
 import com.myspring.daengnyang.member.service.MemberServiceImpl;
-import com.myspring.daengnyang.member.service.OAuthServiceImpl;
+import com.myspring.daengnyang.member.service.OauthServiceImpl;
 import com.myspring.daengnyang.member.vo.MemberInfoVO;
 import com.myspring.daengnyang.member.vo.MemberVO;
 import com.myspring.daengnyang.member.vo.SignupForm;
@@ -31,13 +31,10 @@ public class MemberController {
     private final MemberServiceImpl memberService;
     private final PasswordEncoder passwordEncoder;
 
-    private final OAuthServiceImpl oAuthService;
-
     @Autowired
-    public MemberController(MemberServiceImpl memberService, PasswordEncoder passwordEncoder, OAuthServiceImpl oAuthService) {
+    public MemberController(MemberServiceImpl memberService, PasswordEncoder passwordEncoder, OauthServiceImpl oAuthService) {
         this.memberService = memberService;
         this.passwordEncoder = passwordEncoder;
-        this.oAuthService = oAuthService;
     }
 
     /**
@@ -131,106 +128,6 @@ public class MemberController {
         log.info("내가 쓴 글 불러 오기 실행 / Param => memberNo : " + memberNo);
         return memberService.getMemberPost(memberNo);
     }
-
-    //-------------------------------------------------------------------------------------------
-
-    /**
-     * access token을 통해 카카오 개인정보 체크
-     *
-     * @param code https://kauth.kakao.com/oauth/authorize?client_id=db0c282555cc32e78ecbce031761fc83&redirect_uri=http://localhost:8080/member/oauth/kakao&response_type=code
-     *             <p>
-     *             https://kauth.kakao.com/oauth/logout?client_id=db0c282555cc32e78ecbce031761fc83&logout_redirect_uri=http://localhost:8080/member/oauth/kakao/logout
-     */
-    @GetMapping("/oauth/kakao")
-    public boolean kakaoCallback(@RequestParam String code, HttpServletRequest httpRequest) {
-        log.info("code : " + code);
-        String accessToken = oAuthService.getKakaoAccessToken(code);
-        System.out.println(accessToken);
-        String loginResult = oAuthService.getUserInfo(accessToken);
-        log.info("로그인 정보 : " + loginResult);
-        String memberNo = oAuthService.kakaoLogin(loginResult);
-        if (memberNo != null) {
-            HttpSession session = httpRequest.getSession();
-            session.setAttribute("memberNo", memberNo);
-            log.info("로그인이 정상 처리 되었습니다.");
-            return true;
-        } else {
-            log.info("로그인 오류 발생");
-            return false;
-        }
-    }
-
-    @GetMapping("/oauth/kakao/logout")
-    public void kakaoLogoutCallback() {
-        Long target_id = 3428886536L;
-        log.info("kakaoLogout");
-        String logoutResult = oAuthService.kakaoLogout(target_id);
-        log.info(logoutResult + "로그아웃이 정상 처리 되었습니다.");
-    }
-
-    @GetMapping("oauth/kakao/unlink")
-    public void kakaoUnlinkCallback() { // 회원 탈퇴 시 계정 연결 끊기
-        Long target_id = 3428886536L;
-        log.info("kakaoUnlink");
-        String unLinkResult = oAuthService.kakaoUnlink(target_id);
-        log.info(unLinkResult + "연결 끊기가 정상 처리 되었습니다.");
-    }
-    //---------------------------------------------------------------------------------------------------------------------
-
-    /**
-     * 구글 소셜 로그인
-     */
-
-    @GetMapping("/oauth/google")
-    public boolean googleCallback(@RequestParam String code, HttpServletRequest httpRequest) {
-        log.info("code : " + code);
-        String accessToken = oAuthService.getGoogleAccessToken(code);
-        System.out.println(accessToken);
-        String loginResult = String.valueOf(oAuthService.getUserResource(accessToken));
-        log.info("로그인 정보 : " + loginResult);
-        String memberNo = oAuthService.googleLogin(loginResult);
-        if (memberNo != null) {
-            HttpSession session = httpRequest.getSession();
-            session.setAttribute("memberNo", memberNo);
-            log.info("로그인이 정상 처리 되었습니다.");
-            return true;
-        } else {
-            log.info("로그인 오류 발생");
-            return false;
-        }
-    }
-
-    //---------------------------------------------------------------------------------------------------------------------
-
-    /**
-     * 네이버 로그인
-     * @return
-     */
-
-
-
-    @GetMapping("/oauth/naver")
-    public boolean naverCallback(@RequestParam String code, @RequestParam String state, HttpServletRequest httpRequest){
-        log.info("code : " + code + "state : " + state);
-        String accessToken = oAuthService.getNaverAccessToken(code, state);
-        String loginResult = oAuthService.getNaverUserInfo(accessToken);
-        String memberNo = oAuthService.NaverLogin(loginResult);
-        if (memberNo != null) {
-            HttpSession session = httpRequest.getSession();
-            session.setAttribute("memberNo", memberNo);
-            log.info("로그인이 정상 처리 되었습니다.");
-            return true;
-        } else {
-            log.info("로그인 오류 발생");
-            return false;
-        }
-     }
-
-
-
-
-    //---------------------------------------------------------------------------------------------------------------------
-
 
     @GetMapping("/logout")
     public boolean Logout(HttpSession session) throws Exception {
---
+++ src/main/java/com/myspring/daengnyang/member/controller/OauthController.java
@@ -0,0 +1,118 @@
+package com.myspring.daengnyang.member.controller;
+
+import com.myspring.daengnyang.member.service.OauthServiceImpl;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequestMapping("/member")
+public class OauthController {
+
+    private final OauthServiceImpl oAuthService;
+    @Autowired
+    public OauthController(OauthServiceImpl oAuthService) {
+        this.oAuthService = oAuthService;
+    }
+
+    /**
+     * access token을 통해 카카오 개인정보 체크
+     *
+     * @param code https://kauth.kakao.com/oauth/authorize?client_id=db0c282555cc32e78ecbce031761fc83&redirect_uri=http://localhost:8080/member/oauth/kakao&response_type=code
+     *             <p>
+     *             https://kauth.kakao.com/oauth/logout?client_id=db0c282555cc32e78ecbce031761fc83&logout_redirect_uri=http://localhost:8080/member/oauth/kakao/logout
+     */
+    @GetMapping("/oauth/kakao")
+    public boolean kakaoCallback(@RequestParam String code, HttpServletRequest httpRequest) {
+        log.info("code : " + code);
+        String accessToken = oAuthService.getKakaoAccessToken(code);
+        System.out.println(accessToken);
+        String loginResult = oAuthService.getUserInfo(accessToken);
+        log.info("로그인 정보 : " + loginResult);
+        String memberNo = oAuthService.kakaoLogin(loginResult);
+        if (memberNo != null) {
+            HttpSession session = httpRequest.getSession();
+            session.setAttribute("memberNo", memberNo);
+            log.info("로그인이 정상 처리 되었습니다.");
+            return true;
+        } else {
+            log.info("로그인 오류 발생");
+            return false;
+        }
+    }
+
+    @GetMapping("/oauth/kakao/logout")
+    public void kakaoLogoutCallback() {
+        Long target_id = 3428886536L;
+        log.info("kakaoLogout");
+        String logoutResult = oAuthService.kakaoLogout(target_id);
+        log.info(logoutResult + "로그아웃이 정상 처리 되었습니다.");
+    }
+
+    @GetMapping("oauth/kakao/unlink")
+    public void kakaoUnlinkCallback() { // 회원 탈퇴 시 계정 연결 끊기
+        Long target_id = 3428886536L;
+        log.info("kakaoUnlink");
+        String unLinkResult = oAuthService.kakaoUnlink(target_id);
+        log.info(unLinkResult + "연결 끊기가 정상 처리 되었습니다.");
+    }
+    //---------------------------------------------------------------------------------------------------------------------
+
+    /**
+     * 구글 소셜 로그인
+     */
+
+    @GetMapping("/oauth/google")
+    public boolean googleCallback(@RequestParam String code, HttpServletRequest httpRequest) {
+        log.info("code : " + code);
+        String accessToken = oAuthService.getGoogleAccessToken(code);
+        System.out.println(accessToken);
+        String loginResult = String.valueOf(oAuthService.getUserResource(accessToken));
+        log.info("로그인 정보 : " + loginResult);
+        String memberNo = oAuthService.googleLogin(loginResult);
+        if (memberNo != null) {
+            HttpSession session = httpRequest.getSession();
+            session.setAttribute("memberNo", memberNo);
+            log.info("로그인이 정상 처리 되었습니다.");
+            return true;
+        } else {
+            log.info("로그인 오류 발생");
+            return false;
+        }
+    }
+
+    //---------------------------------------------------------------------------------------------------------------------
+
+    /**
+     * 네이버 로그인
+     * @return
+     */
+
+
+
+    @GetMapping("/oauth/naver")
+    public boolean naverCallback(@RequestParam String code, @RequestParam String state, HttpServletRequest httpRequest){
+        log.info("code : " + code + "state : " + state);
+        String accessToken = oAuthService.getNaverAccessToken(code, state);
+        String loginResult = oAuthService.getNaverUserInfo(accessToken);
+        String memberNo = oAuthService.NaverLogin(loginResult);
+        if (memberNo != null) {
+            HttpSession session = httpRequest.getSession();
+            session.setAttribute("memberNo", memberNo);
+            log.info("로그인이 정상 처리 되었습니다.");
+            return true;
+        } else {
+            log.info("로그인 오류 발생");
+            return false;
+        }
+    }
+
+
+
+}
