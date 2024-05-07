c801a18ef7131ae4d9dc369b70198bf0dccbc53f
HyunSoo Park <104089770+ChordCon@users.noreply.github.com>
2024-05-07 11:30:51 +0900

    modified:   src/main/frontend/src/community/freeBoard.js 	modified:   src/main/frontend/src/community/petBoast.js 	modified:   src/main/frontend/src/community/trainingMethod.js 	modified:   src/main/frontend/src/community/usedMarket.js 	new file:   src/main/frontend/src/defaultImgs.js 	modified:   src/main/frontend/src/home/hotPlace.js 	new file:   src/main/frontend/src/imgDate.js 	modified:   src/main/frontend/src/membership/mypages/selectedLocation.js 	modified:   src/main/frontend/src/membership/mypages/writtenByMe.js 	modified:   src/main/frontend/src/placeRecommend/recommendDetail.js

---
+++ src/main/frontend/src/defaultImgs.js
@@ -0,0 +1,14 @@
+const defaultImg = {
+    미용: "https://firebasestorage.googleapis.com/v0/b/dnvillage-e152c.appspot.com/o/image%2F1715043772142?alt=media&token=a07033b6-3885-477c-8a55-ccc4ed305437",
+    박물관문예회관: "https://firebasestorage.googleapis.com/v0/b/dnvillage-e152c.appspot.com/o/image%2F1715043879949?alt=media&token=120a16e8-0a0f-4a67-9c14-dbf05e7327f8",
+    병원: "https://firebasestorage.googleapis.com/v0/b/dnvillage-e152c.appspot.com/o/image%2F1715043931585?alt=media&token=b84115ef-d180-4c89-bc3e-2621202920f4",
+    약국: "https://firebasestorage.googleapis.com/v0/b/dnvillage-e152c.appspot.com/o/image%2F1715043979520?alt=media&token=c6dcf387-e27d-4701-8d74-697403634278",
+    식당: "https://firebasestorage.googleapis.com/v0/b/dnvillage-e152c.appspot.com/o/image%2F1715043960286?alt=media&token=0d1ee85e-33ae-4709-a293-8ff7fc38977a",
+    여행지: "https://firebasestorage.googleapis.com/v0/b/dnvillage-e152c.appspot.com/o/image%2F1715044002785?alt=media&token=34b616ac-5179-4037-9cab-e2297e6edbf2",
+    애견용품: "https://firebasestorage.googleapis.com/v0/b/dnvillage-e152c.appspot.com/o/image%2F1715044022798?alt=media&token=e72cf470-a59c-4a79-8cba-20655fb90149",
+    유치원: "https://firebasestorage.googleapis.com/v0/b/dnvillage-e152c.appspot.com/o/image%2F1715044049564?alt=media&token=e9930afe-df9f-482b-b762-bfb497b56f6d",
+    카페: "https://firebasestorage.googleapis.com/v0/b/dnvillage-e152c.appspot.com/o/image%2F1715044073808?alt=media&token=a94f1057-8f66-434f-9c85-b5e4a17a0027",
+    호텔펜션: "https://firebasestorage.googleapis.com/v0/b/dnvillage-e152c.appspot.com/o/image%2F1715044141756?alt=media&token=3947997e-a902-46bb-9e42-a806c73c6177"
+}
+
+export default defaultImg
--- src/main/frontend/src/membership/mypages/selectedLocation.js
+++ src/main/frontend/src/membership/mypages/selectedLocation.js
@@ -1,11 +1,13 @@
 import styled from "styled-components";
 import { GoDotFill } from "react-icons/go";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import MypageNavbar from './mypageNavbar';
 import { useRecoilValue } from 'recoil';
 import { isDarkAtom, presentPage } from '../../atoms';
 import themes from "../../theme";
 import Pagination from "../../pagination";
+import defaultImg from "../../defaultImgs";
+import hotPlaceArr from "../../imgDate";
 
 const Container = styled.div`
 display: flex;
@@ -16,7 +18,7 @@ min-height: calc(100vh - 229px);
 margin: 10px 6vw;
 display: grid;
 justify-content: center;
-grid-template-columns: repeat(auto-fit, 300px);
+grid-template-columns: repeat(auto-fit,250px);
 grid-auto-rows: minmax(100px, auto);
 gap: 15px;
 `
@@ -46,15 +48,37 @@ font-size: clamp(90%, 1vw, 100%);
 function SelectedLocation() {
     const isDark = useRecoilValue(isDarkAtom);
     const nowPage = useRecoilValue(presentPage);
-    // 더미 데이터
-    const [hotPlaceArr, setHotPlaceArr] = useState([["춘천 삼악산 호수 케이블카", "강원 춘천시 스포츠타운길 245", "리드줄, 매너벨트 필수 착용", "https://files.ban-life.com/content/2024/04/body_1711961501.jpg"], ["청도 프로방스", "경북 청도군 화양읍 이슬미로 272-23", "리쉬필수! 댕댕이들은 모~두 입장 가능!", "https://files.ban-life.com/content/2024/04/body_1712306959.jpg"], ["양평 레몬과오렌지", "경기도 양평군 단월면 양동로 229", "독채 숙소라 마당에서 프라이빗하게 뛰뛰하고 우리끼리 즐길 수 있어요", "https://files.ban-life.com/content/2024/04/body_1712301999.jpg"], ["캔버스 스테이 외관", "부산광역시 해운대구 해운대해변로197번길 13", " 강아지 수영장+루프탑", "https://files.ban-life.com/content/2024/04/body_1712316724.jpg"], ["태안 코리아 플라워 파크", "충남 태안 안면읍 꽃지해안로 400", "견종 무관하게 모두 동반 가능해요", "https://files.ban-life.com/content/2024/04/body_1712595967.jpg"], ["감성스테이 산아래", "충청남도 당진시 송산면 칠절길 95-17", "견종, 무게 제한 없음", "https://files.ban-life.com/content/2024/04/body_1712079373.jpg"], ["감성스테이 산아래", "충청남도 당진시 송산면 칠절길 95-17", "견종, 무게 제한 없음", "https://files.ban-life.com/content/2024/04/body_1712079373.jpg"], ["감성스테이 산아래", "충청남도 당진시 송산면 칠절길 95-17", "견종, 무게 제한 없음", "https://files.ban-life.com/content/2024/04/body_1712079373.jpg"], ["감성스테이 산아래", "충청남도 당진시 송산면 칠절길 95-17", "견종, 무게 제한 없음", "https://files.ban-life.com/content/2024/04/body_1712079373.jpg"], ["감성스테이 산아래", "충청남도 당진시 송산면 칠절길 95-17", "견종, 무게 제한 없음", "https://files.ban-life.com/content/2024/04/body_1712079373.jpg"], ["감성스테이 산아래", "충청남도 당진시 송산면 칠절길 95-17", "견종, 무게 제한 없음", "https://files.ban-life.com/content/2024/04/body_1712079373.jpg"], ["감성스테이 산아래", "충청남도 당진시 송산면 칠절길 95-17", "견종, 무게 제한 없음", "https://files.ban-life.com/content/2024/04/body_1712079373.jpg"], ["감성스테이 산아래", "충청남도 당진시 송산면 칠절길 95-17", "견종, 무게 제한 없음", "https://files.ban-life.com/content/2024/04/body_1712079373.jpg",], ["춘천 삼악산 호수 케이블카", "강원 춘천시 스포츠타운길 245", "리드줄, 매너벨트 필수 착용", "https://files.ban-life.com/content/2024/04/body_1711961501.jpg"], ["청도 프로방스", "경북 청도군 화양읍 이슬미로 272-23", "리쉬필수! 댕댕이들은 모~두 입장 가능!", "https://files.ban-life.com/content/2024/04/body_1712306959.jpg"], ["양평 레몬과오렌지", "경기도 양평군 단월면 양동로 229", "독채 숙소라 마당에서 프라이빗하게 뛰뛰하고 우리끼리 즐길 수 있어요", "https://files.ban-life.com/content/2024/04/body_1712301999.jpg"], ["캔버스 스테이 외관", "부산광역시 해운대구 해운대해변로197번길 13", " 강아지 수영장+루프탑", "https://files.ban-life.com/content/2024/04/body_1712316724.jpg"], ["태안 코리아 플라워 파크", "충남 태안 안면읍 꽃지해안로 400", "견종 무관하게 모두 동반 가능해요", "https://files.ban-life.com/content/2024/04/body_1712595967.jpg"], ["춘천 삼악산 호수 케이블카", "강원 춘천시 스포츠타운길 245", "리드줄, 매너벨트 필수 착용", "https://files.ban-life.com/content/2024/04/body_1711961501.jpg"], ["청도 프로방스", "경북 청도군 화양읍 이슬미로 272-23", "리쉬필수! 댕댕이들은 모~두 입장 가능!", "https://files.ban-life.com/content/2024/04/body_1712306959.jpg"], ["양평 레몬과오렌지", "경기도 양평군 단월면 양동로 229", "독채 숙소라 마당에서 프라이빗하게 뛰뛰하고 우리끼리 즐길 수 있어요", "https://files.ban-life.com/content/2024/04/body_1712301999.jpg"], ["캔버스 스테이 외관", "부산광역시 해운대구 해운대해변로197번길 13", " 강아지 수영장+루프탑", "https://files.ban-life.com/content/2024/04/body_1712316724.jpg"], ["태안 코리아 플라워 파크", "충남 태안 안면읍 꽃지해안로 400", "견종 무관하게 모두 동반 가능해요", "https://files.ban-life.com/content/2024/04/body_1712595967.jpg"], ["감성스테이 산아래", "충청남도 당진시 송산면 칠절길 95-17", "견종, 무게 제한 없음", "https://files.ban-life.com/content/2024/04/body_1712079373.jpg"], ["감성스테이 산아래", "충청남도 당진시 송산면 칠절길 95-17", "견종, 무게 제한 없음", "https://files.ban-life.com/content/2024/04/body_1712079373.jpg"], ["감성스테이 산아래", "충청남도 당진시 송산면 칠절길 95-17", "견종, 무게 제한 없음", "https://files.ban-life.com/content/2024/04/body_1712079373.jpg"], ["감성스테이 산아래", "충청남도 당진시 송산면 칠절길 95-17", "견종, 무게 제한 없음", "https://files.ban-life.com/content/2024/04/body_1712079373.jpg"], ["감성스테이 산아래", "충청남도 당진시 송산면 칠절길 95-17", "견종, 무게 제한 없음", "https://files.ban-life.com/content/2024/04/body_1712079373.jpg"], ["감성스테이 산아래", "충청남도 당진시 송산면 칠절길 95-17", "견종, 무게 제한 없음", "https://files.ban-life.com/content/2024/04/body_1712079373.jpg"], ["감성스테이 산아래", "충청남도 당진시 송산면 칠절길 95-17", "견종, 무게 제한 없음", "https://files.ban-life.com/content/2024/04/body_1712079373.jpg"], ["감성스테이 산아래", "충청남도 당진시 송산면 칠절길 95-17", "견종, 무게 제한 없음", "https://files.ban-life.com/content/2024/04/body_1712079373.jpg",], ["춘천 삼악산 호수 케이블카", "강원 춘천시 스포츠타운길 245", "리드줄, 매너벨트 필수 착용", "https://files.ban-life.com/content/2024/04/body_1711961501.jpg"], ["청도 프로방스", "경북 청도군 화양읍 이슬미로 272-23", "리쉬필수! 댕댕이들은 모~두 입장 가능!", "https://files.ban-life.com/content/2024/04/body_1712306959.jpg"], ["양평 레몬과오렌지", "경기도 양평군 단월면 양동로 229", "독채 숙소라 마당에서 프라이빗하게 뛰뛰하고 우리끼리 즐길 수 있어요", "https://files.ban-life.com/content/2024/04/body_1712301999.jpg"], ["캔버스 스테이 외관", "부산광역시 해운대구 해운대해변로197번길 13", " 강아지 수영장+루프탑", "https://files.ban-life.com/content/2024/04/body_1712316724.jpg"], ["태안 코리아 플라워 파크", "충남 태안 안면읍 꽃지해안로 400", "견종 무관하게 모두 동반 가능해요", "https://files.ban-life.com/content/2024/04/body_1712595967.jpg"], ["춘천 삼악산 호수 케이블카", "강원 춘천시 스포츠타운길 245", "리드줄, 매너벨트 필수 착용", "https://files.ban-life.com/content/2024/04/body_1711961501.jpg"], ["청도 프로방스", "경북 청도군 화양읍 이슬미로 272-23", "리쉬필수! 댕댕이들은 모~두 입장 가능!", "https://files.ban-life.com/content/2024/04/body_1712306959.jpg"], ["양평 레몬과오렌지", "경기도 양평군 단월면 양동로 229", "독채 숙소라 마당에서 프라이빗하게 뛰뛰하고 우리끼리 즐길 수 있어요", "https://files.ban-life.com/content/2024/04/body_1712301999.jpg"], ["캔버스 스테이 외관", "부산광역시 해운대구 해운대해변로197번길 13", " 강아지 수영장+루프탑", "https://files.ban-life.com/content/2024/04/body_1712316724.jpg"], ["태안 코리아 플라워 파크", "충남 태안 안면읍 꽃지해안로 400", "견종 무관하게 모두 동반 가능해요", "https://files.ban-life.com/content/2024/04/body_1712595967.jpg"], ["감성스테이 산아래", "충청남도 당진시 송산면 칠절길 95-17", "견종, 무게 제한 없음", "https://files.ban-life.com/content/2024/04/body_1712079373.jpg"], ["감성스테이 산아래", "충청남도 당진시 송산면 칠절길 95-17", "견종, 무게 제한 없음", "https://files.ban-life.com/content/2024/04/body_1712079373.jpg"], ["감성스테이 산아래", "충청남도 당진시 송산면 칠절길 95-17", "견종, 무게 제한 없음", "https://files.ban-life.com/content/2024/04/body_1712079373.jpg"], ["감성스테이 산아래", "충청남도 당진시 송산면 칠절길 95-17", "견종, 무게 제한 없음", "https://files.ban-life.com/content/2024/04/body_1712079373.jpg"], ["감성스테이 산아래", "충청남도 당진시 송산면 칠절길 95-17", "견종, 무게 제한 없음", "https://files.ban-life.com/content/2024/04/body_1712079373.jpg"], ["감성스테이 산아래", "충청남도 당진시 송산면 칠절길 95-17", "견종, 무게 제한 없음", "https://files.ban-life.com/content/2024/04/body_1712079373.jpg"], ["감성스테이 산아래", "충청남도 당진시 송산면 칠절길 95-17", "견종, 무게 제한 없음", "https://files.ban-life.com/content/2024/04/body_1712079373.jpg"], ["감성스테이 산아래", "충청남도 당진시 송산면 칠절길 95-17", "견종, 무게 제한 없음", "https://files.ban-life.com/content/2024/04/body_1712079373.jpg",], ["춘천 삼악산 호수 케이블카", "강원 춘천시 스포츠타운길 245", "리드줄, 매너벨트 필수 착용", "https://files.ban-life.com/content/2024/04/body_1711961501.jpg"], ["청도 프로방스", "경북 청도군 화양읍 이슬미로 272-23", "리쉬필수! 댕댕이들은 모~두 입장 가능!", "https://files.ban-life.com/content/2024/04/body_1712306959.jpg"], ["양평 레몬과오렌지", "경기도 양평군 단월면 양동로 229", "독채 숙소라 마당에서 프라이빗하게 뛰뛰하고 우리끼리 즐길 수 있어요", "https://files.ban-life.com/content/2024/04/body_1712301999.jpg"], ["캔버스 스테이 외관", "부산광역시 해운대구 해운대해변로197번길 13", " 강아지 수영장+루프탑", "https://files.ban-life.com/content/2024/04/body_1712316724.jpg"], ["태안 코리아 플라워 파크", "충남 태안 안면읍 꽃지해안로 400", "견종 무관하게 모두 동반 가능해요", "https://files.ban-life.com/content/2024/04/body_1712595967.jpg"]])
+    const [windowSize, setWindowSiz] = useState(window.innerWidth);
+    const handleResize = () => {
+        setWindowSiz(window.innerWidth)
+        console.log(window.innerWidth)
+    }
+    useEffect(() => {
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.addEventListener('resize', handleResize)
+        }
+    }, [])
+    const showImg = (e) => {
+        if (e === "미용") return defaultImg.미용
+        if (e === "박물관문예회관") return defaultImg.박물관문예회관
+        if (e === "병원") return defaultImg.병원
+        if (e === "약국") return defaultImg.약국
+        if (e === "식당") return defaultImg.식당
+        if (e === "여행지") return defaultImg.여행지
+        if (e === "애견용품") return defaultImg.애견용품
+        if (e === "유치원") return defaultImg.유치원
+        if (e === "카페") return defaultImg.카페
+        if (e === "호텔펜션") return defaultImg.호텔펜션
+    }
 
     const totalPost = hotPlaceArr.length; // 총 게시물 수
-    const pageRange = 10; // 페이지당 보여줄 게시물 수
+    const pageRange = 12; // 페이지당 보여줄 게시물 수
     const totalPageNum = Math.ceil(hotPlaceArr.length / pageRange)
     const btnRange = 5; // 보여질 페이지 버튼의 개수
     const startPost = (nowPage - 1) * pageRange + 1; // 시작 게시물 번호
     const endPost = startPost + pageRange - 1; // 끝 게시물 번호
+
     return (
         <div>
             <MypageNavbar />
@@ -62,11 +86,14 @@ function SelectedLocation() {
                 color: `${isDark ? themes.dark.color : themes.light.color}`,
                 backgroundColor: `${isDark ? themes.dark.bgColor : themes.light.bgColor}`
             }}>
-                < PlaceItems >
+                < PlaceItems style={{
+                    gridTemplateColumns: windowSize > 1790 ?
+                        "repeat(auto-fit,250px)" : "repeat(auto-fit,350px)"
+                }} >
                     {hotPlaceArr.slice(startPost - 1, endPost).map((e, i) => (
                         <PlaceItem key={i}>
                             <PlaceItemTitle>{e[0]}</PlaceItemTitle>
-                            <PlaceItemImg style={{ backgroundImage: `url(${e[3]})` }} />
+                            <PlaceItemImg style={{ backgroundImage: `url(${showImg(e[3])})` }} />
                             <PlaceItemAddress><GoDotFill />{e[1]}</PlaceItemAddress>
                             <PlaceItemInfo><GoDotFill />{e[2]}</PlaceItemInfo>
                         </PlaceItem>
