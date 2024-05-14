e0eeaf0d23a9c9b15c25cfac604ec6695fd161c1
HyunSoo Park <104089770+ChordCon@users.noreply.github.com>
2024-05-14 14:45:52 +0900

    modified:   src/main/frontend/src/aboutUs/aboutUs.js 	new file:   src/main/frontend/src/aboutUs/aboutUsInfo.js 	modified:   src/main/frontend/src/home/hotPlace.js 	modified:   src/main/frontend/src/membership/mypages/myInfo.js 	modified:   src/main/frontend/src/membership/mypages/myInfoChange.js 	modified:   src/main/frontend/src/placeRecommend/recommendDetail.js

---
+++ src/main/frontend/src/aboutUs/aboutUsInfo.js
@@ -0,0 +1,37 @@
+const usInfo = [{
+    name: "백진욱",
+    img: "",
+    position: "조장, 백엔드",
+    aboutMe: "",
+    Contact: ""
+},
+{
+    name: "송민영",
+    img: "",
+    position: "백엔드",
+    aboutMe: "",
+    Contact: ""
+},
+{
+    name: "박현수",
+    img: "",
+    position: "프론트 매니저, 프론트엔드",
+    aboutMe: "",
+    Contact: ""
+},
+{
+    name: "이상빈",
+    img: "",
+    position: "프론트엔드",
+    aboutMe: "",
+    Contact: ""
+},
+{
+    name: "정승호",
+    img: "",
+    position: "프론트엔드",
+    aboutMe: "",
+    Contact: ""
+}]
+
+export default usInfo
